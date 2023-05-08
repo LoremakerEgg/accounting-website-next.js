80c78dd8dfb105447c73a7207a536eeacafa7b5c
Anton <antonkoller@outlook.com>
2023-05-08 09:18:33 +0200

    altered default to template

---
+++ app/components/Header.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import Link from "next/link";
+import styles from "./header.module.scss";
+import { usePathname } from "next/navigation";
+
+const Header = () => {
+  const pathname = usePathname();
+  return (
+    <header className={styles.header}>
+      <div>
+        <div className={styles.title}>
+          <Link
+            href="/"
+            className={pathname == "/" ? "myLinkActive" : "myLink"}
+          >
+            TITLE GOES HERE
+          </Link>
+        </div>
+        <div className={styles.links}>
+          <Link
+            href="/about"
+            className={pathname == "/about" ? "myLinkActive" : "myLink"}
+          >
+            Om mig
+          </Link>
+          <Link
+            href="/pricing"
+            className={pathname == "/pricing" ? "myLinkActive" : "myLink"}
+          >
+            Priser
+          </Link>
+          <Link
+            href="/contact"
+            className={pathname == "/contact" ? "myLinkActive" : "myLink"}
+          >
+            Kontakta mig
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
