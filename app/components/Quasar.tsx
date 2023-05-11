c24e996fdf26c7b8ec1346f54744ec8079f1d66a
Anton <antonkoller@outlook.com>
2023-05-11 10:04:39 +0200

    full quasar implementation, needs styling

--- app/components/Quasar.tsx
+++ app/components/Quasar.tsx
@@ -1,12 +1,15 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./quasar.module.scss";
 
 export default function Quasar() {
   const [mainNumber, setMainNumber] = useState(0);
   const [payout, setPayout] = useState(0);
   const [credits, setCredits] = useState(200);
+  const [start, setStart] = useState(1);
+  const [gameOver, setGameOver] = useState(0);
+  const randomNumberStart = Math.floor(Math.random() * 2) + 1;
 
   const clickFourSeven = () => {
     const randomFourSeven = Math.floor(Math.random() * 4) + 4;
@@ -19,6 +22,13 @@ export default function Quasar() {
   };
 
   const clickPayOut = () => {
+    setCredits(credits + payout);
+    setMainNumber(0);
+    setPayout(0);
+    setStart(1);
+  };
+
+  useEffect(() => {
     if (mainNumber === 17) {
       setPayout(200);
     } else if (mainNumber === 18) {
@@ -27,10 +37,10 @@ export default function Quasar() {
       setPayout(300);
     } else if (mainNumber === 20) {
       setPayout(400);
+    } else if (mainNumber > 20) {
+      setGameOver(1);
     }
-    setMainNumber(0);
-    setCredits(credits + payout);
-  };
+  }, [mainNumber]);
 
   const creditPayout = () => {
     console.log(credits);
@@ -41,28 +51,77 @@ export default function Quasar() {
     return credits;
   };
 
-  return (
-    <main className={styles.main}>
-      <div className={styles.divGame}>
-        <h1 className={styles.number}>{mainNumber}</h1>
-        <div className={styles.buttonDiv}>
-          <button onClick={clickOneEight} className={styles.button}>
-            Add 1 - 8
-          </button>
-          <button onClick={clickPayOut} className={styles.button}>
-            Pay Out
-          </button>
-          <button onClick={clickFourSeven} className={styles.button}>
-            Add 4 - 7
-          </button>
+  const clickStart = () => {
+    setStart(0);
+    setGameOver(0);
+    setCredits(credits - 200);
+    setMainNumber(randomNumberStart);
+  };
+
+  if (start === 1) {
+    return (
+      <main className={styles.main}>
+        <div className={styles.divGame}>
+          <h1 className={styles.number}>{mainNumber}</h1>
+          <div className={styles.buttonDiv}>
+            <button onClick={clickStart} className={styles.button}>
+              Play Quasar (200 credits)
+            </button>
+          </div>
         </div>
-      </div>
-      <div>
-        <h2>Payout: {creditPayout()}</h2>
-      </div>
-      <div>
-        <h3>Credits: {creditAccount()} </h3>
-      </div>
-    </main>
-  );
+        <div>
+          <h2>Payout: {creditPayout()}</h2>
+        </div>
+        <div>
+          <h1>Credits: {creditAccount()} </h1>
+        </div>
+      </main>
+    );
+  } else if (gameOver === 1) {
+    return (
+      <main className={styles.main}>
+        <div className={styles.divGame}>
+          <h1 className={styles.number}>
+            Too high! You lose. Press below to play again.
+          </h1>
+          <div className={styles.buttonDiv}>
+            <button onClick={clickStart} className={styles.button}>
+              Play Quasar (200 credits)
+            </button>
+          </div>
+        </div>
+        <div>
+          <h2>Payout: {creditPayout()}</h2>
+        </div>
+        <div>
+          <h1>Credits: {creditAccount()} </h1>
+        </div>
+      </main>
+    );
+  } else {
+    return (
+      <main className={styles.main}>
+        <div className={styles.divGame}>
+          <h1 className={styles.number}>{mainNumber}</h1>
+          <div className={styles.buttonDiv}>
+            <button onClick={clickOneEight} className={styles.button}>
+              Add 1 - 8
+            </button>
+            <button onClick={clickPayOut} className={styles.button}>
+              Pay Out
+            </button>
+            <button onClick={clickFourSeven} className={styles.button}>
+              Add 4 - 7
+            </button>
+          </div>
+        </div>
+        <div>
+          <h2>Payout: {creditPayout()}</h2>
+        </div>
+        <div>
+          <h3>Credits: {creditAccount()} </h3>
+        </div>
+      </main>
+    );
+  }
 }
